029ef3ac80bd074d20d092e89e9492516f81107c
Mary Larrousse <larroussemary@gmail.com>
2019-12-22 19:27:59 -0500

    try a handler

--- AddLootRecordHandler.go
+++ AddLootRecordHandler.go
@@ -1,8 +1,23 @@
 package Openapi
 
+import "time"
+import "database/sql"
+import "log"
+
+var (
+	connectionName = mustGetenv("CLOUDSQL_CONNECTION_NAME")
+	user           = mustGetenv("CLOUDSQL_USER")
+	password       = os.Getenv("CLOUDSQL_PASSWORD")
+)
+
 func AddLootRecord(ctx echo.Context) error {
-	// do we have the datetime?
-	// get psql credentials from env vars
+	// get date time
+	now = time.Now()
+	// db connect
+	db, err = sql.Open("psql", fmt.Sprintf("%s:%s@cloudsql(%s)/", user, password, connectionName))
+	if err ! = nil {
+		log.Fatalf("Could not open db: %v", err)
+	}
 	// make psql insert call
 	// return ok
 }
