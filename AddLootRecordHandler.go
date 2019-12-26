985dcfb13c978eb72280b3895ba582979f382c5e
Mary Larrousse <larroussemary@gmail.com>
2019-12-26 16:09:22 -0500

    alr handler static

--- AddLootRecordHandler.go
+++ AddLootRecordHandler.go
@@ -8,6 +8,7 @@ var (
 	connectionName = mustGetenv("CLOUDSQL_CONNECTION_NAME")
 	user           = mustGetenv("CLOUDSQL_USER")
 	password       = os.Getenv("CLOUDSQL_PASSWORD")
+	tableName	   = mustGetenv("TABLE_NAME")
 )
 
 func AddLootRecord(ctx echo.Context) error {
@@ -18,6 +19,17 @@ func AddLootRecord(ctx echo.Context) error {
 	if err ! = nil {
 		log.Fatalf("Could not open db: %v", err)
 	}
+	//get datas from ctx
+	//TODO
 	// make psql insert call
+	//username submit_timestamp loot[id][amt] mission_id
+	sqlStatement := `
+	INSERT INTO $1 (username, submit_timestamp, loot, mission_id)
+	VALUES ($2, $3, $4, $5)`
+	_, err = db.Exec(sqlStatement, tableName, "mary", '2016-06-22 19:10:25-07', "{{1,2}}", "1")
+	if err != nil {
+		panic(err)
+	}
 	// return ok
+	return ctx.JSON(http.StatusOK, "HOKAYDOKAY")
 }
